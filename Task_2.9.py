10aac4cf0ef8ed7ed47843d6e33bd835ec9868fc
EdDy4ka <laedo.2004@mail.ru>
2023-07-07 02:40:20 +0700

    Pt_2_9: Full task
    The program finds the maximum digit in the number.
    The number itself consists of various digits.
    
    The program counts in two ways:
    From the beggining and from the end.

---
+++ Task_2.9.py
@@ -0,0 +1,11 @@
+natural_number = input("Enter a natural number with different digits: ")
+list_natural_number = list(natural_number)
+max_number = max(list_natural_number)
+
+for i in range(len(list_natural_number)):
+    if list_natural_number[i] == max_number:
+        print(i+1)
+
+for i in range(len(list_natural_number)-1, -1, -1):
+    if list_natural_number[i] == max_number:
+        print(i+1)
