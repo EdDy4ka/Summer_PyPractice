6d5746ff544d278455e0210adcc24ccc61f1706d
EdDy4ka <laedo.2004@mail.ru>
2023-07-07 01:03:04 +0700

    Pt_1_2: Full task
    The program outputs the user's first and last name.
    As well as its lenght.

---
+++ Task_1.2.py
@@ -0,0 +1,6 @@
+name = str(input("Enter your name: "))
+surname = str(input("Enter your surname: "))
+person = str(" ".join([name, surname]))
+
+print("The first and last name are as follows:", person)
+print("Line Length:", len(person))
