4e0bf36212e7efbb79cc08264a71ca48353960ae
EdDy4ka <laedo.2004@mail.ru>
2023-07-12 00:35:29 +0700

    Pt_4_1: Full task
    The program makes the maximum number of digits of a given number.

---
+++ Task_4.1.py
@@ -0,0 +1,13 @@
+number = ''.join(str(input("Enter a number: ")))
+len_number = len(number)
+list_number = []
+list_max_number = []
+
+for i in range(len_number):
+    list_number.append(number[i])
+
+for i in range(len_number):
+    list_max_number.append(max(list_number))
+    list_number.remove(list_max_number[i])
+
+print("The maximum possible number of combinations of numbers:", ''.join(list_max_number))
