ebc215551ce6d4f6af740b89a893f38095307d3f
EdDy4ka <laedo.2004@mail.ru>
2023-07-16 22:20:20 +0700

    Pt_5_1: Full task
    The program also creates a csv-file my favorite books.
    Update: now the program outputs a line that says this.

--- Task_5.1.py
+++ Task_5.1.py
@@ -10,3 +10,5 @@ with open('Favorite_books.csv', 'w', newline='') as file:
     writer_book.writerow({"Book": "Player's handbook", "Author": "Jeremy Crawford", "Year of release": "2021"})
     writer_book.writerow({"Book": "Captain Grant's Children", "Author": "Jules Verne", "Year of release": "2021"})
     writer_book.writerow({"Book": "Fullmetal Alchemist (01)", "Author": "Hiromu Arakawa", "Year of release": "2019"})
+
+print("The csv-file has been created and is ready to work!")
