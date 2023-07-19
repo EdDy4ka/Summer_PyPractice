26f42c38520f0501270a2a4ba685bd1bfb8981bf
EdDy4ka <laedo.2004@mail.ru>
2023-07-19 20:55:21 +0700

    Pt_6_1: Full task
    The program parses the top ten works of the artist.
    A platform called "Yandex Music" is used.
    
    The user sets a link to log in.
    In case of its malfunction, the code will give an error message.

---
+++ Task_6.1.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = str(input("Enter the link to the artist on the 'Яндекс Музыка' platform: "))
+# url = 'https://music.yandex.ru/artist/4247128/tracks'
+
+try:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    quotes = soup.find_all('div', class_='d-track__name')
+
+    print("The artist 's top ten tracks:")
+    count_track = 1
+
+    for quotes in quotes:
+        print(count_track, ")", quotes.text, sep="")
+        if count_track == 10:
+            break
+        count_track += 1
+
+except requests.exceptions.RequestException as error:
+    raise SystemExit(error)
