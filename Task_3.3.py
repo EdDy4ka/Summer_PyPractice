34dce3c5c6001f63bc94b07da0fda896e1d8ec5c
EdDy4ka <laedo.2004@mail.ru>
2023-07-21 17:14:49 +0700

    name: pycodestyle check
    on: [ push ]
    
    jobs:
      check_files:
        runs-on: ubuntu-latest
        steps:
        - uses: actions/checkout@v3
        - name: Python setup
          uses: actions/setup-python@v4
          with:
            python-version: 3.9
        # install requirements
        - name: Install dependencies
          run: |
            python -m pip install --upgrade pip
            if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
        # install pycodestyle
        - name: install pycodestyle
          run: pip install pycodestyle
        # check .py files
        - name: pycodestyle check
          run: pycodestyle --max-doc-length=120 --select E,W ./

--- Task_3.3.py
+++ Task_3.3.py
@@ -1,3 +1,4 @@
 number = int(input("Enter any natural integer: "))
-result_number = lambda x: f"The number {x} is even" if x % 2 == 0 else f"The number {x} is odd"
+result_number = lambda x: f"The number {x} is even"\
+    if x % 2 == 0 else f"The number {x} is odd"
 print(result_number(number))
--- Task_3.8.py
+++ Task_3.8.py
@@ -2,6 +2,6 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 dict_letters = {}
 
 for i in range(len(alphabet)):
-    dict_letters[alphabet[i]] = i+1
+    dict_letters[alphabet[i]] = i + 1
 
 print("Dictionary output:", dict_letters)
