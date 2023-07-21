1b8293311046ba22b958a5cde67ffa50c80f7382
EdDy4ka <laedo.2004@mail.ru>
2023-07-21 15:17:53 +0700

    Pt_1: Error correction
    Pt_1.3:
    Line 5 has been shortened.
    Variables are reassigned.
    
    Pt_1.9:
    The multiplication sign was separated by spaces.

--- Task_1.3.py
+++ Task_1.3.py
@@ -1,13 +1,14 @@
-the_first_number = input("Enter the first number: ")
-the_second_number = input("Enter the second number: ")
-the_third_number = input("Enter the third number: ")
+first_number = input("Enter the first number: ")
+second_number = input("Enter the second number: ")
+third_number = input("Enter the third number: ")
 
-print("The largest number:", max([the_first_number, the_second_number, the_third_number]))
+print("The largest number:", max([first_number, second_number, third_number]))
 
-if the_first_number < the_second_number:
-    the_first_number, the_second_number = the_second_number, the_first_number
-if the_second_number < the_third_number:
-    the_second_number, the_third_number = the_third_number, the_second_number
-if the_first_number < the_second_number:
-    the_first_number, the_second_number = the_second_number, the_first_number
-print(the_first_number, the_second_number, the_third_number)
+if first_number < second_number:
+    first_number, second_number = second_number, first_number
+if second_number < third_number:
+    second_number, third_number = third_number, second_number
+if first_number < second_number:
+    first_number, second_number = second_number, first_number
+
+print(first_number, second_number, third_number)
--- Task_1.9.py
+++ Task_1.9.py
@@ -1,3 +1,3 @@
 a = int(input("Enter the first number 'a': "))
 b = int(input("Enter the second number 'b': "))
-print("Произведение чисел ", a, " и ", b, " равно ", a*b, sep="")
+print("Произведение чисел ", a, " и ", b, " равно ", a * b, sep="")
